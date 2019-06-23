671cab174ca3fcbe1ee7b9a0565e3e9a4dfefcb9
ShahlaMirzayeva96 <49427424+ShahlaMirzayeva96@users.noreply.github.com>
2019-06-24 00:57:37 +0400

    Add files via upload

---
+++ 21.06.19/bosluq-sayi/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         string  stri = "Cavide Eshq olsun Cavide Eshq olsun";
+            EmptyString(stri);
+        }
+        static int EmptyString(string str)
+        {
+            int count = 0;
+           char CountEmpty = ' ';
+        
+            for (int i = 0; i < str.Length; i++)
+               
+            {
+                if (str[i] == CountEmpty)
+                {
+                    count++;
+                }
+            }
+
+
+            Console.WriteLine(count);
+            return count;
+        }
+    }
+}
---
+++ 21.06.19/ededi-orta/ededi-orta/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arry = { 2, 4, 8,19,29 };
+            MiddleNum(arry);
+        }
+        static int MiddleNum (int[] arr)
+        { int result = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                    result +=arr[i];
+             
+                
+            }
+            
+            Console.WriteLine(result/arr.Length);
+
+
+
+            return result;
+        }
+    }
+}
